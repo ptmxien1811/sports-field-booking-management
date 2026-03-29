fd7def4711ed5cccec2821dc7fe813c4154f8607
lydailongdichmam123-hue <lydailongdichmam123@gmail.com>
2026-03-29 13:45:10 +0700

    FE KhamPha

---
+++ bookingapp/static/js/explore.js
@@ -0,0 +1,15 @@
+    // TAB chuyển nội dung
+    const tabs = document.querySelectorAll(".nav-tabs a");
+    const contents = document.querySelectorAll(".tab-content");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", (e) => {
+            e.preventDefault();
+            tabs.forEach(t => t.classList.remove("active"));
+            tab.classList.add("active");
+            contents.forEach(c => c.classList.remove("active"));
+            const target = tab.dataset.target;
+            const el = document.getElementById(target);
+            if (el) el.classList.add("active");
+        });
+    });
